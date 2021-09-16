08768e9a4ea0a544d0f255216d7c67bed43359da
Antonio <68387604+antonio-1337@users.noreply.github.com>
2021-09-16 23:20:26 +0200

    added coefficients function

--- Cognome_Nome_matricola_mvpoly_LP_202109/Prolog/mvpoly.pl
+++ Cognome_Nome_matricola_mvpoly_LP_202109/Prolog/mvpoly.pl
@@ -35,12 +35,12 @@ is_var_power_list(VPs) :-
   forall(member(VP, VPs), is_var_power(VP)).
 
 %%get_pwr_from_vp/1
-% prende in input un VarPower e ne restituisce l'esponente
+%prende in input un VarPower e ne restituisce l'esponente
 get_pwr_from_vp(Vp, Result) :-
  Vp =.. TempList,
  last(TempList, Result).
 
-%%get_td/2
+%%get_monomial_degree/2
 %prende come primo parametro la lista di VarPowers e mette il grado totale
 %nel secondo parametro
 get_monomial_degree([], 0).
@@ -49,13 +49,43 @@ get_monomial_degree([Vp|Vps], TotalSum) :-
   get_pwr_from_vp(Vp, Pow),
   TotalSum is Pow + PartialSum.
 
+%%get_monomial_coef/2
+%prende come primo parametro un Monomial e mette il  suo coefficiente nel
+%nel secondo parametro
+get_monomial_coef(m(C, _, _), C) :- !.
+
 %%is_polinomial/1
 is_polinomial(poly(Monomials)) :-
   is_list(Monomials),
   forall(member(M, Monomials), is_monomial(M)).
 
 %%is_zero/1
-% ritorna true per tutte le rappresenzazioni definite dello "0"
+%ritorna true per tutte le rappresenzazioni definite dello "0"
 is_zero(X) :- X == 0, !.
-is_zero(X) :- X == m(0, 0, []) , !.
+is_zero(X) :-
+  is_monomial(X),
+  get_monomial_coef(X, C),
+  C == 0, !.
 is_zero(X) :- X == poly([]), !.
+is_zero(poly([X])) :-
+  is_zero(X), !.
+
+%%get_poli_coeffs/2
+%usato da coefficients/2
+get_poli_coeffs(poly([]), []) :- !.
+get_poli_coeffs(poly(Monomials), CoeffList) :-
+  get_poli_coeffsCall(poly(Monomials), CoeffList).
+
+%%get_poli_coeffsCall/2
+%usato da get_poli_coeffs/2
+get_poli_coeffsCall(poly([]), []) :- !.
+get_poli_coeffsCall(poly([M|Ms]), [C|Cs]) :-
+  get_monomial_coef(M, C),
+  get_poli_coeffsCall(poly(Ms), Cs).
+
+%%coefficients/2
+%ritorna true quando vero quando Coefficients è una lista dei
+%coefficienti di Poly
+coefficients([], []).
+coefficients(poly(Monomials), CoefList) :-
+  get_poli_coeffs(poly(Monomials), CoefList).
